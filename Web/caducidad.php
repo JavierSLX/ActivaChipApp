4fb058fef2fb0c28fc59b0aa0371de5f1cef8209
JavierSLX <jserranolule@gmail.com>
2017-12-02 14:36:17 -0600

    Correccion de utf8

--- Web/caducidad.php
+++ Web/caducidad.php
@@ -29,6 +29,7 @@ $result = reporteCaducidad($usuarioID,$inicio,$TAMANO_PAGINA);
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+	<meta charset="UTF-8" />
 	<title>ActivaChip</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<link rel="stylesheet" type="text/css" href="css/bootstrap/css/bootstrap.min.css">
--- Web/reporte.php
+++ Web/reporte.php
@@ -15,6 +15,7 @@ $datosUsuario=$sesion->datosUsuario();
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
+	<meta charset="UTF-8" />
 	<title>ActivaChip</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<link rel="stylesheet" type="text/css" href="css/bootstrap/css/bootstrap.min.css">
@@ -112,7 +113,7 @@ if (empty($fechaIn && $fechafi)){
 							echo "<td height = 10>"."   ".$row[1]."   "."</td>";
 							echo "<td height = 10>"."   ".$row[2]."   "."</td>";
 							echo "<td height = 10>"."$".$row[3]."   "."</td>";
-							echo "<td height = 10>"."   ".$row[4]."   "."</td>";
+							echo "<td height = 10>"."   ".utf8_encode($row[4])."   "."</td>";
 							echo "</tr>";
 				  }
 
